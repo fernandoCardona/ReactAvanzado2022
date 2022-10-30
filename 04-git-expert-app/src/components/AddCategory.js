6fc14af237bf7a2405442afeeb2c7e2e13cc6f0b
fernandoCardona <fernando.cardona@the-lion.com>
2022-10-30 23:33:49 +0100

    change in AddCategory component

--- 04-git-expert-app/src/components/AddCategory.js
+++ 04-git-expert-app/src/components/AddCategory.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const AddCategory = ({ setCategories } ) => {
+export const AddCategory = ( { setCategories } ) => {
     const [inputValue, setInputValue] = useState('');
     const handleInputChange = (e) => {
         setInputValue( e.target.value );
