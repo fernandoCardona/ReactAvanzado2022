4c3066ecc94a2d566f63d21471c30a7f3af22893
fernandoCardona <fernando.cardona@the-lion.com>
2022-11-08 22:26:56 +0100

    animate.css

--- 06-heroes-app/src/selectors/getHeroesByPublisher.js
+++ 06-heroes-app/src/selectors/getHeroesByPublisher.js
@@ -2,7 +2,7 @@ import { heroes } from "../data/heroes";
 
 export const getHeroesByPublisher = ( publisher ) => {
     const validPublishers = ['DC Comics','Marvel Comics']; 
-    if(!validPublishers.includes( publisher )) {
+    if( !validPublishers.includes( publisher ) ) {
          throw new Error (`Publisher "${ publisher }" no es correcto`);
     }
 
