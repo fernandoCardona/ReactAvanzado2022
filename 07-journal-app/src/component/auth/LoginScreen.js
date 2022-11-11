259a4c7e8e85339a2e227acca5b035e8991eba0e
fernandoCardona <fernando.cardona@the-lion.com>
2022-11-11 18:51:12 +0100

    btn google login page

--- 07-journal-app/src/component/auth/LoginScreen.js
+++ 07-journal-app/src/component/auth/LoginScreen.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginScreen = () => {
   return (
     <div>
-        <h1>LoginScreen</h1>
+        <>
+          <h3 className="auth__title">Login</h3>
+          <form action="" className="">
+            <input  
+              typet="email"
+              placeholder="Email"
+              name="email"
+              className="auth__input"
+            />
+            <input 
+              type="password"
+              placeholder="Password" 
+              name="password"
+              className="auth__input"
+            />
+            <button
+              type="submit"
+              className="btn btn-primary btn-block"
+              //disabled={true}
+            >
+              Login
+            </button>
+
+           
+            <div className="auth__social-networks">
+              <p>Login With social network</p>
+              <div className="google-btn" >
+                <div className="google-icon-wrapper">
+                    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                </div>
+                <p className="btn-text">
+                    <b>Sign in with google</b>
+                </p>
+            </div>
+            </div>
+            
+            <Link 
+              to="/auth/register" 
+              className="btn"
+            >
+              Create new account
+            </Link>
+
+          </form>
+        </>
     </div>
   )
 }
